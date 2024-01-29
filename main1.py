621721cfba8bcb6290d02794fb9eb9e366a51f5d
AKASH <87299311+gamkers@users.noreply.github.com>
2024-01-29 09:42:29 +0530

    Update main1.py

--- main1.py
+++ main1.py
@@ -2,7 +2,6 @@ import streamlit as st
 import google.generativeai as genai
 
 
-genai.configure(api_key="AIzaSyByN6qu5xoyB4asbKH-IT2VMBeA0mToHDU")
 def ai(txt):
     
     for m in genai.list_models():
